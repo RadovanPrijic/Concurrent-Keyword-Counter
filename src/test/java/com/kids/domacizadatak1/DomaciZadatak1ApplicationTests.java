45577f59779e4564175c2e2f630bf8da68428c07
RadovanPrijic <rprijic3619rn@raf.rs>
2023-03-21 16:19:14 +0100

    Inicijalni commit

---
+++ src/test/java/com/kids/domacizadatak1/DomaciZadatak1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kids.domacizadatak1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomaciZadatak1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
